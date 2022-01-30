c67bcaa436479f4d711b989bf0e1f3237b2a688e
thisisommore <ommore501@gmail.com>
2022-01-30 06:29:41 +0000

    Address lint issues

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import { BlockerClient } from "../generated/grpc/protos/BlockerServiceClientPb"
-import { GetAuthTokenRequest } from "../generated/grpc/protos/getauthtoken/getauthtoken_pb"
-import * as grpcWeb from "grpc-web"
+// import { GetAuthTokenRequest } from "../generated/grpc/protos/getauthtoken/getauthtoken_pb"
+// import * as grpcWeb from "grpc-web"
 
 const blockerservice = new BlockerClient(process.env.REACT_SERVER_URL ?? "http://localhost:8080");
-
+export default blockerservice

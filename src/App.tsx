d83a335c69ada10060e323bfd7e48198c3f6bafe
thisisommore <ommore501@gmail.com>
2022-01-31 04:43:08 +0000

    Remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Card from "./Component/Card/Card";
-// import SearchBar from "./Component/Search/SearchBar";
 import "./api/index";
 import SignIn from "./pages/signin/SignIn";
 

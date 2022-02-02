c517f2dbbb7b109dfe82f44fe34d322738d09af7
Om More <51229945+thisisommore@users.noreply.github.com>
2022-02-02 22:25:07 +0530

    gitpod: fix prebuild

--- generate_protos.sh
+++ generate_protos.sh
@@ -1,5 +1,7 @@
 mkdir -p src/generated/grpc
-cd src/generated && svn checkout https://github.com/NetBlockGit/dns-server/trunk/protos
+cd src/generated
+svn checkout https://github.com/NetBlockGit/dns-server/trunk/protos \
+                    --username=$GITPOD_GIT_USER_NAME --password=$GITHUB_TOKEN
 protoc -I=. protos/blocker.proto \
                 --js_out=import_style=commonjs,binary:grpc \
                 --grpc-web_out=import_style=typescript,mode=grpcwebtext:grpc

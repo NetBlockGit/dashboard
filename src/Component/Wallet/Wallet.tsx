fce4c7ed5b8baa4c11b6f4b7ece53c9f1cea9ec2
kunal232i <kingawale23@gmail.com>
2022-02-12 18:45:25 +0000

    Wallet Connect component

---
+++ src/Component/Wallet/Wallet.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import StyleWallet from "./StyleWallet";
+
+type props = { placeholder: string; [key: string]: any };
+
+export default function Wallet(p: props): React.ReactElement {
+  return (
+    <StyleWallet>
+      <div className="boxcontainer">
+        <div className="Wallet">
+          <span className="iconify" data-icon="logos:metamask-icon"></span>
+          <span>{p.placeholder}</span>
+        </div>
+      </div>
+    </StyleWallet>
+  );
+}
